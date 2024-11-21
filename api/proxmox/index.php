0d0ec92913bb2425efc92f5012c68f543087e044
INMD1 <lyw514549@gmail.com>
2024-11-21 05:37:23 +0000

    Vender 패키지 설치

---
+++ api/proxmox/index.php
@@ -0,0 +1,33 @@
+<?php
+require(__DIR__ . '/../../vendor/autoload.php');
+
+use Proxmox\Request;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+header('Content-Type: application/json');
+
+$parameter = $_GET["search"] ?? null;
+
+$configure = [
+    'hostname' => '10.10.2.1',
+    'username' => 'aksmdncc',
+    'password' => "kcearqjtnjv5rvy7wegkdgilexocli0m58kcaffsnk6ywpge81"
+];
+
+try {
+    Request::Login($configure); 
+
+    switch ($parameter) {
+        case 'nodes':
+            $response = Request::Request('/nodes', null, 'GET');
+            echo json_encode($response);
+            break;
+
+        default:
+            echo json_encode(['error' => "No valid parameter provided!"]);
+            break;
+    }
+} catch (Exception $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
