5528f2296fed33bb5233b89874f5b937d485b2b8
INMD1 <lyw514549@gmail.com>
2024-11-25 19:12:03 +0900

    사ㅌ이트 올리기

--- frontend/src/Part_compomnet/Mainpage/Mainpage_index.tsx
+++ frontend/src/Part_compomnet/Mainpage/Mainpage_index.tsx
@@ -53,7 +53,8 @@ function Mainpage_index() {
     useEffect(() => {
         async function fetchData() {
             try {
-                const response = await fetch(process.env.API_URL + "/api/proxmox?search=nodes");
+                
+                const response = await fetch("https://phpproject.powerinmd.com/api/proxmox?search=nodes");
                 const data = await response.json();
 
                 // 데이터 기반으로 상태 업데이트
--- frontend/src/Part_compomnet/auth/Auth.tsx
+++ frontend/src/Part_compomnet/auth/Auth.tsx
@@ -7,12 +7,63 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input";
-
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Label } from "@radix-ui/react-label";
 
+
+const FormSchema = z.object({
+  name: z.string().min(1, {
+    message: "이름이 입력되지 않았습니다.",
+  }),
+  email: z.string().min(1, {
+    message: "이메일이 입력되지 않랐습니다.",
+  }),
+  password: z.string().min(8, {
+    message: "비빌번호가 8자리 이상이 아님니다",
+  }),
+  student_class: z.string().min(1, {
+    message: "학과가 입력되지 않았습니다.",
+  }),
+  student_ID: z.string().min(7, {
+    message: "학번이 입력되지 않았습니다.",
+  }),
+  phone_number: z.string().min(13, {
+    message: "전화번호가 입력되지 않았습니다. - 도 추가해야 합니다.",
+  }),
+})
+
 function Auth() {
+
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    //@ts-ignore
+    name: "",
+    email: "",
+    password: "",
+    student_class: "",
+    student_ID: "",
+    phone_number: ""
+  })
+
+  function onSubmit(values: z.infer<typeof FormSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
   return (
     <>
       <div className="grid  place-content-center">
@@ -20,7 +71,7 @@ function Auth() {
           <div className="h-[10vh] lg:h-[10vh]"></div>
           <Tabs
             defaultValue="account"
-            className="pl-[8vh] lg:pl-0 w-[80vw] lg:w-[35vw]"
+            className="pl-[8vh] lg:pl-0 w-[80vw] lg:w-[30vw]"
           >
             <TabsList className="grid w-full grid-cols-2 lg:h-[4vh]">
               <TabsTrigger value="account" className="authtitle  lg:h-[3.3vh]">
@@ -69,42 +120,103 @@ function Auth() {
                     바람니다.
                   </CardDescription>
                 </CardHeader>
-                <CardContent className="space-y-2">
-                  <form>
-                    <div className="grid w-full items-center gap-4">
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">이름</Label>
-                        <Input id="email_login" type="email" placeholder="" />
+                <CardContent>
+                  <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+                      <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>이름</FormLabel>
+                            <FormControl>
+                              <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="email"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>이메일</FormLabel>
+                            <FormControl>
+                              <Input placeholder="example@Test.com" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>비밀번호</FormLabel>
+                            <FormControl>
+                              <Input placeholder="영문숫자 포함 8자리 이상" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="student_class"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>학과</FormLabel>
+                            <FormControl>
+                              <Input placeholder="shadcn" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="student_ID"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>학번</FormLabel>
+                            <FormControl>
+                              <Input placeholder="00000000" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <FormField
+                        control={form.control}
+                        name="phone_number"
+                        render={({ field }) => (
+                          <FormItem>
+                            <FormLabel>핸드폰 전화번호</FormLabel>
+                            <FormControl>
+                              <Input placeholder="010-0000-0000" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                          </FormItem>
+                        )}
+                      />
+                      <br />
+                      <div className="items-top flex space-x-2">
+                        <Checkbox id="terms1" />
+                        <div className="grid gap-1.5 leading-none">
+                          <label
+                            htmlFor="terms1"
+                            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                          >
+                            개인정보수집에 동의합니다.
+                          </label>
+                        </div>
                       </div>
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">Email</Label>
-                        <Input id="name" placeholder="" />
-                      </div>
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">비밀번호</Label>
-                        <Input
-                          id="name"
-                          placeholder="영문숫자포함 8자리 이상"
-                        />
-                      </div>
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">학과</Label>
-                        <Input id="student_class" placeholder="" />
-                      </div>
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">학번</Label>
-                        <Input id="studentID" placeholder="" />
-                      </div>
-                      <div className="flex flex-col space-y-1.5">
-                        <Label htmlFor="name">전화번호</Label>
-                        <Input id="phone_number" placeholder="" />
-                      </div>
-                    </div>
-                    <br />
-                    <CardFooter className="p-3 lg:p-3">
-                      <Button className="singup w-full">회원가입</Button>
-                    </CardFooter>
-                  </form>
+                      <br />
+                      <Button type="submit" className="singup w-full">회원가입</Button>
+                    </form>
+                  </Form>
                 </CardContent>
               </Card>
             </TabsContent>
