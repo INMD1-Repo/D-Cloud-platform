2da482503e1345a5eb262236037b979fe9d5fcf7
INMD1 <87979171+INMD1@users.noreply.github.com>
2025-01-08 13:06:11 +0900

    [추가] 사용자 정보는 미리 입력되게 수정함

--- frontend/src/Part_compomnet/Server application/Main-server.tsx
+++ frontend/src/Part_compomnet/Server application/Main-server.tsx
@@ -36,6 +36,7 @@ import { Checkbox } from "@radix-ui/react-checkbox";
 import { useAtom } from "jotai";
 import { login_Count, User_info } from "@/store/strore_data";
 import { useNavigate } from "react-router-dom";
+import { Label } from "@/components/ui/label";
 
 const source = `
 # 서비스 이용약관
@@ -111,6 +112,18 @@ const source = `
 7. 기존에 서버에 저장되어 있던 데이터가 어떠한 이유로든 상실된 경우, 이에 대한 책임은 전적으로 고객에게 있습니다. 기관은 이로 인해 발생하는 어떠한 손해에 대해서도 책임을 지지 않습니다.
 `;
 
+interface userinfo {
+  email: string;
+  name: string;
+  phone_number: string;
+  password: string;
+  student_id: string;
+  created_at: string;
+  updated_at: null;
+  student_class: string;
+  Admin: number;
+}
+
 const FormSchema = z.object({
   name: z.string().min(1, "이름은 필수 항목입니다."),
   email: z.string().email("유효한 이메일을 입력하세요."),
@@ -131,10 +144,10 @@ const FormSchema = z.object({
 });
 
 const options = [
-  { value: "ubuntu24", label: "Ubuntu 24.04.1" },
-  { value: "ubuntu22", label: "Ubuntu 22.04.5" },
-  { value: "ubuntu20", label: "Ubuntu 20.04.6" },
-  { value: "ubuntu18", label: "Ubuntu 18.04.6" },
+  { value: "ubuntu24", label: "Ubuntu 24.04.1 LTS" },
+  { value: "ubuntu22", label: "Ubuntu 22.04.5 LTS" },
+  { value: "ubuntu20", label: "Ubuntu 20.04.6 LTS" },
+  { value: "ubuntu18", label: "Ubuntu 18.04.6 LTS" },
   { value: "debian", label: "Debian 12.8.0" },
   { value: "rocky9", label: "Rocky 9.5" },
   { value: "rocky8", label: "Rocky 8.9" },
@@ -147,7 +160,8 @@ function Main_server({ className }: React.HTMLAttributes<HTMLDivElement>) {
   });
   const navigate = useNavigate();
   const [userinfo] = useAtom(User_info);
-  const info = userinfo;
+  //@ts-ignore
+  const info: userinfo = userinfo;
 
   const [logCount] = useAtom(login_Count);
 
@@ -155,9 +169,9 @@ function Main_server({ className }: React.HTMLAttributes<HTMLDivElement>) {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
     defaultValues: {
-      name: "",
-      email: "",
-      phone_number: "",
+      name: info.name,
+      email: info.email,
+      phone_number: info.phone_number,
       Application_period: "",
       Reason_for_renta: "",
       Servername: "",
@@ -260,49 +274,12 @@ function Main_server({ className }: React.HTMLAttributes<HTMLDivElement>) {
                 <div className="grid md:flex gap-x-8 grow">
                   <div className="grid">
                     <p className="flex server_sub_sub_title">사용자 정보</p>
-                    <FormField
-                      control={form.control}
-                      name="name"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>이름</FormLabel>
-                          <FormControl>
-                            <Input placeholder="이름" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    ></FormField>
-                    <FormField
-                      control={form.control}
-                      name="email"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>이메일</FormLabel>
-                          <FormControl>
-                            <Input
-                              placeholder="이메일"
-                              type="email"
-                              {...field}
-                            />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    ></FormField>
-                    <FormField
-                      control={form.control}
-                      name="phone_number"
-                      render={({ field }) => (
-                        <FormItem>
-                          <FormLabel>전화번호</FormLabel>
-                          <FormControl>
-                            <Input placeholder="전화번호" {...field} />
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    ></FormField>
+                    <Label htmlFor="terms">이름</Label>
+                    <Input placeholder={info.name} disabled />
+                    <Label htmlFor="terms">이메일</Label>
+                    <Input placeholder={info.email} disabled />
+                    <Label htmlFor="terms">전화번호</Label>
+                    <Input placeholder={info.phone_number} disabled />
                     <p className="flex server_sub_sub_title">대여 정보</p>
                     <div className={cn("grid gap-2", className)}>
                       <Popover>
